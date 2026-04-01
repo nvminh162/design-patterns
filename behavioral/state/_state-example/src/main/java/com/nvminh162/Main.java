02fce5f8c3f129ff490dc351ba790a381d3c6a9d
Nguyen Van Minh <nvminh162@gmail.com>
2026-04-01 17:41:52 +0700

    m

--- behavioral/state/_state-example/src/main/java/com/nvminh162/Main.java
+++ behavioral/state/_state-example/src/main/java/com/nvminh162/Main.java
@@ -4,10 +4,16 @@ public class Main {
     public static void main(String[] args) {
         VendingMachine machine = new VendingMachine();
 
-        machine.insertCoin();
+        System.out.println("============");
         machine.selectProduct();
-
+        System.out.println("============");
         machine.insertCoin();
+        System.out.println("============");
+        machine.selectProduct();
+        System.out.println("============");
         machine.insertCoin();
+        System.out.println("============");
+        machine.insertCoin();
+        System.out.println("============");
     }
 }
